4758081870733abdc45ab978160a8f970ec0354b
NikosZoros3533 <nikos7kozani@gmail.com>
2026-06-10 16:54:22 +0300

    BYTEPATH#2 #Input

--- main.lua
+++ main.lua
@@ -1,11 +1,54 @@
--- function love.load()
---
--- end
---
--- function love.update()
---
--- end
+Object = require("libraries/classic/classic")
+
+function love.load()
+	local object_files = {}
+	recursiveEnumerate("objects", object_files)
+	requireFiles(object_files)
+	circle = Circle(400, 300, 50)
+	hyperCircle = HyperCircle(400, 300, 50, 10, 120)
+end
+
+function love.update(dt)
+	circle:update(dt)
+	hyperCircle:update(dt)
+end
 
 function love.draw()
-	love.graphics.print("Hello Worlds", 400, 300)
+	circle:draw()
+	hyperCircle:draw()
+end
+
+function love.keypressed(key)
+	print(key)
+end
+
+function love.keyreleased(key)
+	print(key)
+end
+
+function love.mousepressed(x, y, button)
+	print(x, y, button)
+end
+
+function love.mousereleased(x, y, button)
+	print(x, y, button)
+end
+
+function recursiveEnumerate(folder, file_list)
+	local items = love.filesystem.getDirectoryItems(folder)
+	for _, item in ipairs(items) do
+		local file = folder .. "/" .. item
+		if love.filesystem.getInfo(file) then
+			table.insert(file_list, file)
+		elseif love.filesystem.isDirectory(file) then
+			recursiveEnumerate(file, file_list)
+		end
+	end
+end
+
+function requireFiles(files)
+	for _, file in ipairs(files) do
+		local file = file:sub(1, -5)
+		require(file)
+	end
 end
